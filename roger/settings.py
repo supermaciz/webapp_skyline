0cbc6b82355ff259ec78131a8de5a12324844f72
root <root@Source>
2016-11-24 14:05:43 +0100

    static files

--- roger/settings.py
+++ roger/settings.py
@@ -135,7 +135,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "collected_static")
+#STATIC_ROOT = os.path.join(BASE_DIR, "collected_static")
+STATIC_ROOT = "/opt/webapp/static"
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
