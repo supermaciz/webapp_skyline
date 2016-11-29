c41c4acf2afecb86dca6add6596f6e8bff2552b9
Mathieu Cizo <maciz@outlook.fr>
2016-11-29 21:23:05 +0100

    Test mail

--- connection/tests.py
+++ connection/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+"Tests. Useful for the CI-CD stuff"
 
-# Create your tests here.
+import os
+from django.test import TestCase
+from .views import send_email
+
+
+class ViewTests(TestCase):
+    "..."
+    def test_send_email(self):
+        "Fake email to test this feature"
+        with self.settings(EMAIL_BACKEND='django.core.mail.backends.filebased.EmailBackend',
+                           EMAIL_FILE_PATH='/tmp/django-mail666'):
+            try:
+                os.remove('/tmp/django-mail666')
+            except FileNotFoundError:
+                pass
+            send_email("haha@yo.test", "Le message", "sujet")
+            try:
+                with open('/tmp/django-mail666'):
+                    mail_exists = True
+            except FileNotFoundError:
+                mail_exists = False
+            self.assertIs(mail_exists, True)
