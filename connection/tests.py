9ea65c0e73f4754aedfec41833dbb990e837c9ca
mcizo <maciz@outlook.fr>
2016-11-30 00:43:35 +0100

    correct mail test

--- connection/tests.py
+++ connection/tests.py
@@ -1,6 +1,7 @@
 "Tests. Useful for the CI-CD stuff"
 
 import os
+from shutil import rmtree
 from django.test import TestCase
 from .views import send_email
 
@@ -10,16 +11,17 @@ class ViewTests(TestCase):
     def test_send_email(self):
         "Fake email to test this feature"
         with self.settings(EMAIL_BACKEND='django.core.mail.backends.filebased.EmailBackend',
-                           EMAIL_FILE_PATH='/tmp/django-mail666',
-                           DATABASES=TEST_DB):
+                           EMAIL_FILE_PATH='/tmp/django-mail666'):
             try:
-                os.remove('/tmp/django-mail666')
+                rmtree("/tmp/django-mail666")
             except FileNotFoundError:
                 pass
             send_email("haha@yo.test", "Le message", "sujet")
             try:
-                with open('/tmp/django-mail666'):
+                if os.listdir('/tmp/django-mail666'):
                     mail_exists = True
+                else:
+                    mail_exists = False
             except FileNotFoundError:
                 mail_exists = False
             self.assertIs(mail_exists, True)
