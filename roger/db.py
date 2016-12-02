2b40b20ec3a910fddde1d97768dd02e9a79b5463
Axel KAZIAN <akazian@e3r5p10.42.fr>
2016-12-02 15:18:38 +0100

    add db test lock

--- roger/db.py
+++ roger/db.py
@@ -1,8 +1,16 @@
+from pathlib import Path
+
 
 class   ReadOrWrite(object):
 
     def db_for_read(self, model, **hints):
-        return "db_read"
+        if Path('/tmp/db.lock').exists():
+            return "db_read"
+        else:
+            return "db_read"
 
     def db_for_write(self, model, **hints):
-        return "default"
+        if Path('/tmp/db.lock').exists():
+            return "default"
+        else:
+            return "default"
